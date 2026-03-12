f0d1c674b859981db587f32034c20e923ea8bb96
itzzavy <maheshchoudhary591@gmail.com>
2026-03-13 03:24:21 +0530

    Delete reportLimitSchema.js

--- reportLimitSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const reportLimitSchema = new mongoose.Schema({
-userId: String,
-reportCount: Number,
-resetTime: Number
-});
-
-module.exports = mongoose.model("ReportLimit", reportLimitSchema);
