963e9213c6c60d079e29f66255754aaa4b847466
itzzavy <maheshchoudhary591@gmail.com>
2026-03-13 03:35:40 +0530

    Create sticky.js

---
+++ sticky.js
@@ -0,0 +1,107 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = (client, db) => {
+
+const stickyCollection = db.collection("stickyMessages");
+
+client.once("clientReady", async () => {
+
+const commands = [
+
+new SlashCommandBuilder()
+.setName("sticky")
+.setDescription("Create a sticky message")
+.addChannelOption(option =>
+option.setName("channel")
+.setDescription("Channel for sticky")
+.setRequired(true))
+.addStringOption(option =>
+option.setName("message")
+.setDescription("Sticky message text")
+.setRequired(true)),
+
+new SlashCommandBuilder()
+.setName("unsticky")
+.setDescription("Remove sticky message")
+.addChannelOption(option =>
+option.setName("channel")
+.setDescription("Channel")
+.setRequired(true))
+
+];
+
+for (const guild of client.guilds.cache.values()) {
+await guild.commands.create(commands[0]);
+await guild.commands.create(commands[1]);
+}
+
+});
+
+client.on("interactionCreate", async interaction => {
+
+if (!interaction.isChatInputCommand()) return;
+
+if (interaction.commandName === "sticky") {
+
+const channel = interaction.options.getChannel("channel");
+const message = interaction.options.getString("message");
+
+await stickyCollection.updateOne(
+{ channelId: channel.id },
+{
+$set: {
+channelId: channel.id,
+message: message,
+lastMessageId: null
+}
+},
+{ upsert: true }
+);
+
+interaction.reply({ content: "Sticky message created.", ephemeral: true });
+
+}
+
+if (interaction.commandName === "unsticky") {
+
+const channel = interaction.options.getChannel("channel");
+
+await stickyCollection.deleteOne({ channelId: channel.id });
+
+interaction.reply({ content: "Sticky removed.", ephemeral: true });
+
+}
+
+});
+
+client.on("messageCreate", async message => {
+
+if (message.author.bot) return;
+
+const data = await stickyCollection.findOne({ channelId: message.channel.id });
+
+if (!data) return;
+
+try {
+
+if (data.lastMessageId) {
+const oldMsg = await message.channel.messages.fetch(data.lastMessageId).catch(() => null);
+if (oldMsg) await oldMsg.delete();
+}
+
+const embed = new EmbedBuilder()
+.setColor(0x0B3D91)
+.setDescription("📌 " + data.message);
+
+const newSticky = await message.channel.send({ embeds: [embed] });
+
+await stickyCollection.updateOne(
+{ channelId: message.channel.id },
+{ $set: { lastMessageId: newSticky.id } }
+);
+
+} catch (err) {}
+
+});
+
+};
