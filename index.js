a969a2125606e0717bb827e6587dccb1284df6ca
itzzavy <maheshchoudhary591@gmail.com>
2026-03-15 23:23:13 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+require("./database")(client);
+require("./events/ready")(client);
+require("./events/interactionCreate")(client);
+require("./events/messageCreate")(client);
+require("./report")(client);
+
+client.login(process.env.TOKEN);
