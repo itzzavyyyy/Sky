682aba0fa95374997f4b9c8996945b5cffd87070
itzzavy <maheshchoudhary591@gmail.com>
2026-03-27 16:04:48 +0530

    Update emiireact.js

--- events/emiireact.js
+++ events/emiireact.js
@@ -2,6 +2,8 @@ module.exports = (client) => {
 
   const TARGET_USER_ID = "747868137843589160";
   const EMOJI = "<a:a_beardance:1484666741685489795>";
+  const TARGET_USER_ID = "722454528317849711";
+  const EMOJI = "<:mahii_chup_bkl:1159024330760531978>";
 
   client.on("messageCreate", async (message) => {
 
