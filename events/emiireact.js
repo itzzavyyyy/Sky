d8daf994815fb7e2751ade478ae8e6c55f7a1ca2
itzzavy <maheshchoudhary591@gmail.com>
2026-03-27 16:10:11 +0530

    Update emiireact.js

--- events/emiireact.js
+++ events/emiireact.js
@@ -2,7 +2,7 @@ module.exports = (client) => {
 
   const USER_EMOJIS = {
     "747868137843589160": "<a:a_beardance:1484666741685489795>",
-    "722454528317849711": "<:mahii_chup_bkl:1159024330760531978>"
+    "722454528317849711": "<:chup_bkl:1159024330760531978>"
   };
 
   client.on("messageCreate", async (message) => {
