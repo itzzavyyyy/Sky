ae659043ee03715ba9569bcc1ac1e126df5c7936
itzzavy <maheshchoudhary591@gmail.com>
2026-03-27 16:08:24 +0530

    Update emiireact.js

--- events/emiireact.js
+++ events/emiireact.js
@@ -1,19 +1,23 @@
 module.exports = (client) => {
 
-  const TARGET_USER_ID = "747868137843589160";
-  const EMOJI = "<a:a_beardance:1484666741685489795>";
-  const TARGET_USER_ID = "722454528317849711";
-  const EMOJI = "<:mahii_chup_bkl:1159024330760531978>";
+  const USER_EMOJIS = {
+    "747868137843589160": "<a:a_beardance:1484666741685489795>",
+    "722454528317849711": "<:mahii_chup_bkl:1159024330760531978>"
+  };
 
   client.on("messageCreate", async (message) => {
 
     if (!message.guild) return;
 
-    if (message.author.id !== TARGET_USER_ID) return;
+    const emoji = USER_EMOJIS[message.author.id];
+
+    if (!emoji) return; // ignore other users
 
     try {
-      await message.react(EMOJI);
-    } catch {}
+      await message.react(emoji);
+    } catch (err) {
+      console.log("React error:", err);
+    }
 
   });
 
