4d764cb5ed0920ad4064d99c8747fe8b6c51a40c
itzzavy <maheshchoudhary591@gmail.com>
2026-03-27 14:53:07 +0530

    Update statusSystem.js

--- events/statusSystem.js
+++ events/statusSystem.js
@@ -13,7 +13,8 @@ module.exports = (client) => {
         PLAYING: 0,
         STREAMING: 1,
         LISTENING: 2,
-        WATCHING: 3
+        WATCHING: 3,
+        COOKING: 4
       };
 
       try {
